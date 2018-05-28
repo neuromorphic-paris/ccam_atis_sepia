b8d1a1fd26c4acf6216cc7203cc78a0829242ff2
Alexandre Marcireau <alexandre.marcireau@inserm.fr>
2018-05-28 11:46:26 +0200

    Pull the latest sepia version

--- premake4.lua
+++ premake4.lua
@@ -1,65 +1,24 @@
-solution 'ccamAtisSepia'
-    configurations {'Release', 'Debug'}
+solution 'ccam_atis_sepia'
+    configurations {'release', 'debug'}
     location 'build'
-
-    newaction {
-        trigger = "install",
-        description = "Install the library",
-        execute = function()
-            os.copyfile('source/sepia.hpp', '/usr/local/include/ccamAtisSepia.hpp')
-            print(string.char(27) .. '[32mCCam ATIS Sepia library installed.' .. string.char(27) .. '[0m')
-            os.exit()
-        end
-    }
-
-    newaction {
-        trigger = 'uninstall',
-        description = 'Remove all the files installed during build processes',
-        execute = function()
-            os.execute('rm -f /usr/local/include/ccamAtisSepia.hpp')
-            print(string.char(27) .. '[32mCCam ATIS Sepia library uninstalled.' .. string.char(27) .. '[0m')
-            os.exit()
-        end
-    }
-
-    newoption {
-        trigger = 'skip-dependencies',
-        description = 'Do not install the library dependencies'
-    }
-
-    project 'ccamAtisSepiaTest'
-        -- General settings
+    project 'ccam_atis_sepia'
         kind 'ConsoleApp'
         language 'C++'
         location 'build'
-        files {'source/**.hpp', 'test/**.hpp', 'test/**.cpp'}
-
-        -- Define the include paths
-        includedirs {'/usr/local/include'}
-        libdirs {'/usr/local/lib'}
-
-        -- Link to the required libraries
+        files {'source/*.hpp', 'test/*.cpp'}
         links {'usb-1.0'}
-
-        -- Declare the configurations
-        configuration 'Release'
-            targetdir 'build/Release'
+        buildoptions {'-std=c++11'}
+        linkoptions {'-std=c++11'}
+        configuration 'release'
+            targetdir 'build/release'
             defines {'NDEBUG'}
             flags {'OptimizeSpeed'}
-        configuration 'Debug'
-            targetdir 'build/Debug'
+        configuration 'debug'
+            targetdir 'build/debug'
             defines {'DEBUG'}
             flags {'Symbols'}
-
-        -- Linux specific settings
         configuration 'linux'
-            buildoptions {'-std=c++11'}
-            linkoptions {'-std=c++11'}
             links {'pthread'}
-            postbuildcommands {'cp ../source/ccamAtisSepia.hpp /usr/local/include/ccamAtisSepia.hpp'}
-
-        -- Mac OS X specific settings
         configuration 'macosx'
-            buildoptions {'-std=c++11', '-stdlib=libc++'}
-            linkoptions {'-std=c++11', '-stdlib=libc++'}
-            postbuildcommands {'cp ../source/ccamAtisSepia.hpp /usr/local/include/ccamAtisSepia.hpp'}
+            includedirs {'/usr/local/include'}
+            libdirs {'/usr/local/lib'}
